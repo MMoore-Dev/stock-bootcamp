8272fd832e63120a31b57ace1394123ddafd11d3
M.Moore <mancheeta4@gmail.com>
2026-04-03 10:58:18 -0400

    Day 2: added profit calculator script

---
+++ day2_profit_calculator.py
@@ -0,0 +1,14 @@
+buy_price = 100
+sell_price = 115
+shares = 10
+
+total_cost = buy_price * shares
+total_value = sell_price * shares
+profit = total_value - total_cost
+percent_return = (profit/total_cost) * 100
+
+print("-----Trade Summary-----")
+print("Total Cost:", total_cost)
+print("Total Value:", total_value)
+print("Profit/Loss", profit)
+print("Percent Return", round(percent_return, 1), "%")
