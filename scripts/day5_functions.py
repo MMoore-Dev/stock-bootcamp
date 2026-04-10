67e1fc66c6214d695cbb7d4119dcbd25005a15ff
M.Moore <mancheeta4@gmail.com>
2026-04-10 18:29:00 -0400

    used calculate average function in script

--- scripts/day5_functions.py
+++ scripts/day5_functions.py
@@ -1,4 +1,4 @@
-from utils import analyze_trade
+from utils import analyze_trade, calculate_average
 
 #Use the function
 trade = analyze_trade(100, 115, 10)
@@ -6,3 +6,9 @@ trade = analyze_trade(100, 115, 10)
 print("Profit: ", trade["profit"])
 print("Percent return: ", round(trade["percent_return"], 2), "%")
 print("Result: ", trade["result"])
+
+prices = [100,105,108,110,106,101]
+
+#call the calculate average function
+average_price = calculate_average(prices)
+print("Average price: ", round(average_price, 2))
