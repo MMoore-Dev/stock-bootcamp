d16537ed0a0782a477873cc05736124d52c6b828
M.Moore <mancheeta4@gmail.com>
2026-04-20 14:15:54 -0400

    Fixed loop issue and used import module

--- scripts/day5_functions.py
+++ scripts/day5_functions.py
@@ -1,14 +1,34 @@
-from utils import analyze_trade, calculate_average
+# Day 5: Analyze trade function
+from utils import analyze_trade
 
-#Use the function
-trade = analyze_trade(100, 115, 10)
+def calculate_profit(buy_price, sell_price, shares):
+    total_cost = buy_price * shares
+    total_value = sell_price * shares
+    profit = total_value - total_cost
+    return profit
 
-print("Profit: ", trade["profit"])
-print("Percent return: ", round(trade["percent_return"], 2), "%")
-print("Result: ", trade["result"])
+# --- Single Trade Test ---
+trade = analyze_trade(100, 110, 10)
 
-prices = [100,105,108,110,106,101]
+print("Single Trade:")
+print("Profit:", trade["profit"])
+print("Percent Return:", round(trade["percent_return"], 2), "%")
+print("Result:", trade["result"])
 
-#call the calculate average function
-average_price = calculate_average(prices)
-print("Average price: ", round(average_price, 2))
+
+# --- Multiple Trades Test ---
+trades = [
+    (100, 115, 10),
+    (50, 45, 20),
+    (30, 30, 15)
+]
+
+print("\nMultiple Trades:")
+
+for trade in trades:
+    result_data = analyze_trade(*trade)
+
+    print("\nTrade:", trade)
+    print("Profit:", result_data["profit"])
+    print("Percent:", round(result_data["percent_return"], 2), "%")
+    print("Result:", result_data["result"])
